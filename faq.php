8875c09fdd7df76854c23fa26e37e5b3bfa4aced
Giel Goudsmit <giel@goudsmit.tech>
2020-03-20 09:56:10 +0000

    Current state of website as hosted on localheroesonline.com

---
+++ faq.php
@@ -0,0 +1,220 @@
+<!DOCTYPE htm>
+<html lang="en">
+<head>
+  	<title>LocalHeroes | Home</title>
+  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+	<meta name="description" content="">
+	
+	<!-- FONTS -->
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+	
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/layout.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <style type="text/css">
+        .bg-primary {
+    background-color: #006168!important;
+}
+.ft-primary
+{
+    color: #000;
+}
+.tagline
+{
+    font-weight: bold;
+    color: #006168;
+    font-size: 23px;
+}
+
+    </style>
+
+</head>
+
+
+
+
+
+
+
+
+
+<body style="background-color: #f1edea">
+	<div class="container">
+        <div class="row mt-1">
+            <div class="col-md-4 col-4">
+                <img src="http://localheroesonline.com/img/logo1.png" height="40px">
+            </div>
+            <div class="col-md-4 col-5 text-center">
+                <h1 class="tagline my-0">FAQ</h1>
+            </div>
+        </div>
+		 <div class="row">
+    <div class="col-12 mx-auto">
+        
+        <div class="accordion" id="faqExample">
+            <div class="card my-2">
+                <div class="card-header px-2 bg-primary" id="headingOne">
+
+                    <h5 class="mb-0 text-white">
+                        <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i>   Wanneer kan ik afhalen?
+                        </button>
+                    </h5>
+                </div>
+
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#faqExample">
+                    <div class="card-body py-1">
+                        <b>A)</b> Het Pickup punt is geopend tussen 17:00 en 20:00u en bevind zich op de ten Katestraat tegenover Doppio.
+                    </div>
+                </div>
+            </div>
+            <div class="card my-2">
+                <div class="card-header px-2 bg-primary" id="headingTwo">
+                    <h5 class="mb-0 text-white">
+                        <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                           <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i> Ik red het niet om op tijd af te halen!
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#faqExample">
+                    <div class="card-body py-1">
+                        <b>A)</b> We zullen contact met je opnemen om te vragen of je nog komt. In het ergste geval bewaren we de spullen nog een dagje voor je. We kunnen niet garanderen dat je spullen een dag later nog even vers zijn dus probeer dit te voorkomen.
+                    </div>
+                </div>
+            </div>
+            <div class="card my-2">
+                <div class="card-header px-2 bg-primary" id="headingThree">
+                    <h5 class="mb-0 text-white">
+                        <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                          <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i>   Wanneer kan ik bestellen?
+
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                    <div class="card-body py-1">
+                       <b>A)</b> Je kan op winkeldagen tussen 9:00 en 15:00 je producten bestellen. Je hebt besteld als de betaling gedaan is. Je kan wel op andere momenten je producten in je mandje zetten. Je order definitief maken kan alleen tussen 9:00 en 15.00.
+                    </div>
+                </div>
+            </div>
+            <div class="card my-2">
+                <div class="card-header px-2 bg-primary" id="headingThree">
+                    <h5 class="mb-0 text-white">
+                        <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                           <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i>  Er is iets mis met mijn bestelling!
+
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseFour" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                    <div class="card-body py-1">
+                       <b>A)</b> Neem contact met ons op via de whatsapp knop onder het kopje 'meer' en we gaan het voor je oplossen!
+                    </div>
+                </div>
+            </div>
+                <div class="card my-2">
+                    <div class="card-header px-2 bg-primary" id="headingThree">
+                        <h5 class="mb-0 text-white">
+                            <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i>    Hoe kan ik betalen?
+
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseFive" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                        <div class="card-body py-1">
+                            <b>A)</b> Nadat je je bestelling compleet hebt gemaakt kan je je bestelling definitief maken middels een iDeal betaling. Als de betaling voltooid is dan krijg je een bevestiging hiervan per e-mail.
+                        </div>
+                    </div>
+                </div>
+                <div class="card my-2">
+                    <div class="card-header px-2 bg-primary" id="headingThree">
+                        <h5 class="mb-0 text-white">
+                            <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                             <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i> De ondernemer reageert niet?
+
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseSix" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                        <div class="card-body py-1">
+                            <b>A)</b> Je kan op verschillende plaatsen in de app op het Whatsapp icoontje drukken om contact op te nemen met ondernemers of Local Heroes via whatsapp. Als een ondernemer niet reageert op je bericht dan heeft hij het waarschijnlijk heel druk in de winkel. Je kan proberen te bellen of even contact opnemen met Local Heroes bij 'meer'.
+                        </div>
+                    </div>
+                </div>
+                 <div class="card my-2">
+                    <div class="card-header px-2 bg-primary" id="headingThree">
+                        <h5 class="mb-0 text-white">
+                            <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                              <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i> Hoe betalen jullie dit?
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseSeven" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                        <div class="card-body py-1">
+                          <b>A)</b> Als je je spullen af komt halen zijn de producten dezelfde prijs als in de winkel. De ondernemers betalen 10% commissie over order als service fee voor Local Heroes. Dit kan op dit moment nog helemaal niet uit maar we zijn dit model nu voor het eerst aan het testen in Amsterdam Oud West om te kijken of we dit verder kunnen uitrollen.
+                        </div>
+                    </div>
+                </div>
+               
+
+                <div class="card my-2">
+                    <div class="card-header px-2 bg-primary" id="headingThree">
+                        <h5 class="mb-0 text-white">
+                            <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                              <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i> Waarom bestaan jullie?
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseSeven" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                        <div class="card-body py-1">
+                           <b>A)</b> Zowel winkelstraten als markten komen steeds leger te staan. Dit omdat mensen steeds meer bij grote winkels (e.g. Albert Heijn) en online gaan kopen. Dit vindt Local Heroes onwijs zonde want er zijn zoveel leuke gave gedreven ondernemers die ervoor zorgen dat de buurt ook levendig blijft. We zien ook dat deze kleine ondernemers moeilijk meekomen met de veranderende tijden. Ze gaan om 17u dicht en doen niet mee aan koop -avonden en -zondagen. Local Heroes verlengt in feite de openingstijden van deze winkels en zorgt dat het makkelijker wordt voor klanten om te bestellen bij verschillende ondernemers in de buurt.
+                        </div>
+                    </div>
+                </div>
+                <div class="card my-2">
+                    <div class="card-header px-2 bg-primary" id="headingThree">
+                        <h5 class="mb-0 text-white">
+                            <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                              <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i> Hoe krijg ik het juiste gewicht?
+
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseEight" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                        <div class="card-body py-1">
+                           <b>A)</b> De ondernemer is hier altijd zo zorgvuldig mogelijk in maar het lukt niet altijd om exact op je gevraagde gewicht uit te komen. Soms is het iets minder en soms iets meer. Als het minder is krijg je een positief saldo bij je Local Heroes punten en als het meer is een negatief saldo.
+                        </div>
+                    </div>
+                </div>
+                <div class="card my-2">
+                    <div class="card-header px-2 bg-primary" id="headingThree">
+                        <h5 class="mb-0 text-white">
+                            <button class="btn btn-link text-white text-left px-0 ft-sm m-0 py-1 font-weight-bold" type="button" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                             <i class="fa fa-caret-right ft-primary ft-lg mr-1" aria-hidden="true"></i> Wat zijn Local Heroes punten?
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseNine" class="collapse" aria-labelledby="headingThree" data-parent="#faqExample">
+                        <div class="card-body py-1">
+                           <b>A)</b> Local Heroes punten worden gebruikt om dingen te verrekenen. Dit kan zijn omdat het gewicht van wat je besteld heb niet overeenkomt met wat de ondernemer meegegeven heeft. We proberen dit wel altijd zo dichtbij mogelijk te doen. Ook hebben we een statiegeld system met tasjes die op deze manier verrekend worden. Punten worden altijd op je volgende order verrekend en het overzicht is te zien onder 'meer'.
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+</div>
+	</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
