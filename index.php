449adc107243b167f59f80169f08904c7f3511f4
Giel Goudsmit <giel@goudsmit.tech>
2020-03-20 13:40:56 +0100

    Added text and links to Google Forms for zipcodes out of delivery area

--- index.php
+++ index.php
@@ -144,7 +144,11 @@
 					
 					<p id="deliver" style="display:none;color: #525252;font-size: 20px;font-weight: bold;">We bezorgen in jouw buurt! Meld je snel aan voor de app en voltooi je bestelling</p>
 					
-					<p id="notdel" style="display:none;color: #525252;font-size: 20px;font-weight: bold;">Helaas, we bezorgen nog niet bij jou in de straat. Je kan natuurlijk wel gebruik maken van onze afhaal mogelijkheid.</p>
+					<div id="notdel" style="display:none;color: #525252;font-size: 20px;font-weight: bold;">
+                        <p>Helaas, we bezorgen nog niet bij jou in de straat. Je kan natuurlijk wel gebruik maken van onze afhaal mogelijkheid.</p>
+                        <p>Ben je ondernemer of gebiedsmanager en heb je interesse in Local Heroes in jou gebied? Laat dan <a href="https://docs.google.com/forms/d/e/1FAIpQLSd_pTDnVmpzvKVzJNgRzGcFFEefbGain-C6ps7DqjYMd8T29A/viewform" target="_blank">hier</a> je gegevens achter.</p>
+                        <p>Wil je op de hoogte gehouden worden over Local Heroes? Schrijf je dan <a href="https://docs.google.com/forms/d/e/1FAIpQLScnEgIW2av8yiBeJgtw_H68QVsvKOjICPuEANNa54sH93c7TQ/viewform" target="_blank">hier</a> in voor de nieuwsbrief</p>
+                    </div>
 				</div>
 				
 			</div>
