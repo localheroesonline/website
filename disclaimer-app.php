21cb4001077d75a78f76f0365eeeb496f30d1b40
Mick Perez <perez.mick@gmail.com>
2020-10-09 00:16:23 +0200

    Update disclaimer-app.php
    Formatting

--- disclaimer-app.php
+++ disclaimer-app.php
@@ -1,45 +1,55 @@
 <!DOCTYPE htm>
 <html lang="en">
+
 <head>
-  	<title>LocalHeroes | Disclaimer Web</title>
-  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="mobile-web-app-capable" content="yes">
-	<meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
-	<meta name="description" content="">
-	
-	<!-- FONTS -->
-	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
-	
-	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/layout.css">
-	<link rel="stylesheet" type="text/css" href="css/custom.css">
-	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <title>LocalHeroes | Disclaimer Web</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+    <meta name="description" content="">
+
+    <!-- FONTS -->
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/layout.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
 
 </head>
+
 <body style="background-color: #f1edea">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 offset-md-2">
-				<div class="block">
-					<div class="header px-0">
-						<h4 class="title ft-lg fw-600 inline text primary">Disclaimer F.o.R. BV (Local Heroes)</h4>
-						<span class="float-right"><a href="index.php" class="v-btn v-btn-dark btn-sm">Back</a></span>
-					</div>
-					<div class="content skin-white privacy-content bx-shadow" style="padding-bottom: 150px;">
-						<p>De besloten vennootschap Local Heroes (hierna: Local Heroes), met KvK-nummer 73680648, wil u hartelijk welkom heten op deze applicatie (hierna: app). Door deze app te gebruiken stemt u in met deze disclaimer. Local Heroes kan de inhoud van de app en deze disclaimer altijd aanpassen zonder dat u daarover bericht ontvangt. De inhoud van deze app is door Local Heroes zorgvuldig samengesteld, maar gebruik van de app is op eigen risico. Local Heroes zal zich naar haar beste vermogen inspannen om de inhoud van de app zo correct mogelijk te houden, maar staat er niet voor in dat haar app altijd juist of volledig zal zijn. Informatie, dus ook het aanbod van diensten, kan fouten omvatten of achterhaald zijn. Local Heroes is niet aansprakelijk voor schade of andere nadelige gevolgen door gebruik of onbereikbaarheid van (informatie op) deze app. Handelingen die u op basis van deze app of de door Local Heroes verstrekte informatie verricht, komen voor uw eigen rekening en risico. Op grond van die fouten of achterhaalde informatie, kan dus ook geen overeenkomst tussen u en Local Heroes tot stand komen. Als u een klacht heeft, dan hoort Local Heroes dat graag. Stuur uw klacht over de inhoud naar <a href="#" class="link-text fw-600"> info@localheroesonline.nl</a>.</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="block">
+                    <div class="header px-0">
+                        <h4 class="title ft-lg fw-600 inline text primary">Disclaimer F.o.R. BV (Local Heroes)</h4>
+                        <span class="float-right"><a href="index.php" class="v-btn v-btn-dark btn-sm">Back</a></span>
+                    </div>
+                    <div class="content skin-white privacy-content bx-shadow" style="padding-bottom: 150px;">
+                        <p>De besloten vennootschap Local Heroes (hierna: Local Heroes), met KvK-nummer 73680648, wil u hartelijk welkom heten op deze applicatie (hierna: app). Door deze app te gebruiken stemt u in met deze disclaimer. Local Heroes kan de
+                            inhoud van de app en deze disclaimer altijd aanpassen zonder dat u daarover bericht ontvangt. De inhoud van deze app is door Local Heroes zorgvuldig samengesteld, maar gebruik van de app is op eigen risico. Local Heroes zal
+                            zich naar haar beste vermogen inspannen om de inhoud van de app zo correct mogelijk te houden, maar staat er niet voor in dat haar app altijd juist of volledig zal zijn. Informatie, dus ook het aanbod van diensten, kan fouten
+                            omvatten of achterhaald zijn. Local Heroes is niet aansprakelijk voor schade of andere nadelige gevolgen door gebruik of onbereikbaarheid van (informatie op) deze app. Handelingen die u op basis van deze app of de door Local
+                            Heroes verstrekte informatie verricht, komen voor uw eigen rekening en risico. Op grond van die fouten of achterhaalde informatie, kan dus ook geen overeenkomst tussen u en Local Heroes tot stand komen. Als u een klacht heeft,
+                            dan hoort Local Heroes dat graag. Stuur uw klacht over de inhoud naar <a href="#" class="link-text fw-600"> info@localheroesonline.nl</a>.</p>
 
-						<p>U mag deze app gebruiken, maar u kunt geen aanspraak maken op Intellectuele Eigendomsrechten van Local Heroes of die van licentiegevers. Local Heroes behoudt zich alle rechten met betrekking tot (de inhoud van) de app voor. Toestemming voor het gebruik van inhoud van deze app op publiekelijk toegankelijke plaatsen kunt u altijd via <a href="#" class="link-text fw-600">info@localheroesonline.nl</a> aanvragen. </p>
+                        <p>U mag deze app gebruiken, maar u kunt geen aanspraak maken op Intellectuele Eigendomsrechten van Local Heroes of die van licentiegevers. Local Heroes behoudt zich alle rechten met betrekking tot (de inhoud van) de app voor. Toestemming
+                            voor het gebruik van inhoud van deze app op publiekelijk toegankelijke plaatsen kunt u altijd via <a href="#" class="link-text fw-600">info@localheroesonline.nl</a> aanvragen. </p>
 
-						<p>Mocht het zo zijn dat Local Heroes per abuis uw auteursrecht geschonden heeft door het gebruik van artikelen of afbeeldingen, laat het Local Heroes weten via <a href="#" class="link-text fw-600">info@localheroesonline.nl</a>. Eventuele artikelen of afbeeldingen die het auteursrecht schenden, worden dan direct van de app verwijderd, zonder dat Local Heroes verplicht is tot enige schadevergoeding.</p>
-					</div>
-					<div class="footer text-center">
-						
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                        <p>Mocht het zo zijn dat Local Heroes per abuis uw auteursrecht geschonden heeft door het gebruik van artikelen of afbeeldingen, laat het Local Heroes weten via <a href="#" class="link-text fw-600">info@localheroesonline.nl</a>. Eventuele
+                            artikelen of afbeeldingen die het auteursrecht schenden, worden dan direct van de app verwijderd, zonder dat Local Heroes verplicht is tot enige schadevergoeding.</p>
+                    </div>
+                    <div class="footer text-center">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
